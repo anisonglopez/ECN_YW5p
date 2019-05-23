03f621a39a43aad79f22ab71a07d640d3d0ff7f4
phoom <phoom@ch7.com>
2019-05-23 14:13:56 +0700

    send mail

--- views/mail_send/mail_send.php
+++ views/mail_send/mail_send.php
@@ -1,6 +1,7 @@
 <?php 
 require "../../vendor/phpmailer/PHPMailerAutoload.php";
 include "../00_config/connect.php";
+include "../layout/config.php";
 sleep(1);
 // Give security access
 set_time_limit(MAIL_TIMEOUT);
@@ -28,7 +29,41 @@ if(isset($_POST['mail_to'])){
             print "Error!: " . $e->getMessage() . "<br/>";
         }
 }
+        try{
+            $statement = $pdo->prepare("SELECT eff_exp_date_int FROM 00_eff_exp_date ");
+            $statement->execute();
+            $result_eff_exp_date = $statement->fetchAll();
+            foreach ($result_eff_exp_date as $row) :
+                $eff_exp_date_int = $row['eff_exp_date_int'];
+            endforeach;
+            // Get Notification Data
+            $tbl_ecn= '30_ecn';
+            $search_date_start = date("Y/m/d");
+            $search_date_end = date("Y/m/d", strtotime('+'.$eff_exp_date_int.' days'));
 
+        $statement = $pdo->prepare("SELECT *  FROM $tbl_ecn
+        WHERE ecn_trash = 0 
+        AND (eff_date  BETWEEN '$search_date_start' and '$search_date_end' 
+        AND eff = 'Effective'  AND ecn_status = 'Follow_up')
+        OR (ecn_status = 'Follow_up' AND eff = 'Effective')
+        ");
+        $statement->execute();
+        $result_noti = $statement->fetchAll();
+        } //try
+        catch (PDOException $e) {
+        print "Error!: " . $e->getMessage() . "<br/>";
+        }
+        try{
+            $date_yesterday = date("Y/m/d");
+            $date_yesterday = date("Y/m/d", strtotime('-1 days'));
+            $statement = $pdo->prepare("SELECT ecn_no From 30_ecn WHERE created_date = '$date_yesterday' ");
+            $statement->execute();
+            $result_ecn_create_date = $statement->fetchAll();
+            // รายการ ecn ที่ใกล้หมด Effective date
+        } //try
+        catch (PDOException $e) {
+            print "Error!: " . $e->getMessage() . "<br/>";
+        }
 // ดึงข้อมูล Email มาจากตาราง feedback_email โดย avtive = 0
 header('Content-Type: text/html; charset=utf-8');
 $mail = new PHPMailer;
@@ -41,23 +76,22 @@ $mail->SMTPAuth = true;
 $mail->SMTPSecure = false;
 //$mail->SMTPDebug = 2; mailDebug 
  
- 
-$gmail_username = "poomwattanaklang@gmail.com"; // gmail ที่ใช้ส่ง
-$gmail_password = "Anisong1234"; // รหัสผ่าน gmail
 // ตั้งค่าอนุญาตการใช้งานได้ที่นี่ https://myaccount.google.com/lesssecureapps?pli=1
- 
-
  
 $sender = $mail_from; // ชื่อผู้ส่ง
 $email_sender = MAIL_SENDER_ADDRESS; // เมล์ผู้ส่ง 
-$email_receiver = $mail_to; // เมล์ผู้รับ ***
-echo $email_receiver ;
+$email_receiver = explode(',', $mail_to); // เมล์ผู้รับ ***
+//$email_receiver = $mail_to; 
 // $subject = "[Phonebook] ข้อความจากระบบ phonebook"; // หัวข้อเมล์
 $mail->Username = MAIL_USR;
 $mail->Password = MAIL_PWD;
 $mail->setFrom($email_sender, $sender);
-$mail->addAddress('poomwattanaklang@gmail.com');
-$mail->Subject = $subject;
+foreach($email_receiver as $email)
+{
+   $mail->addAddress($email);
+   // เพิ่ม email Address
+}
+$mail->Subject = $subject . ' ประจำวันที่ ' .date('d/m/Y');;
 
 // ส่วนของ
 // header('Content-Type: text/html; charset=utf-8');
@@ -101,83 +135,53 @@ $email_content =' <!DOCTYPE html>
     </head>
     <body style="position: absolute;
     width: 700px;">
-        <h1 style="background: #0460c7;padding: 10px 0 20px 10px;margin-bottom:10px;font-size:30px;color:white;" >
-            ECN Management
-            <hr>
-        </h1>
+        
         <div style="padding:20px;">
             <div>
           
-                <h2>  <strong style="color:#0000ff;">'.nl2br($description).'</strong></h2>
-                    <p>ระบบขอแจ้งให้ทราบว่า มีรายการ ECN รายการใหม่ ของวันที่'.date("d/m/Y" , strtotime("-1days")).'จำนวน 39  รายการ ดังนี้</p>
-                                        <ul class="list-group">
-                                                    <li class="list-group-item">1) 1G700-1953</li>
-                                                    <li class="list-group-item">2) 1G700-1953</li>
-                                                    <li class="list-group-item">3) 1G700-1953</li>
-                                                    <li class="list-group-item">4) 1G700-1953</li>
-                                                    <li class="list-group-item">5) 1G700-1953</li>
-                                                    <li class="list-group-item">6) 1G700-1953</li>
-                                                    <li class="list-group-item">7) 1G700-1953</li>
-                                                    <li class="list-group-item">8) 1G700-1953</li>
-                                                    <li class="list-group-item">9) 1G700-1953</li>
-                                                    <li class="list-group-item">10) 1G700-1953</li>
-                                                    <li class="list-group-item">11) 1G700-1953</li>
-                                                    <li class="list-group-item">12) 1G700-1953</li>
-                                                    <li class="list-group-item">13) 1G700-1953</li>
-                                                    <li class="list-group-item">14) 1G700-1953</li>
-                                                    <li class="list-group-item">15) 1G700-1953</li>
-                                                    <li class="list-group-item">16) 1G700-1953</li>
-                                                    <li class="list-group-item">17) 1G700-1953</li>
-                                                    <li class="list-group-item">18) 1G700-1953</li>
-                                                    <li class="list-group-item">19) 1G700-1953</li>
-                                                    <li class="list-group-item">20) 1G700-1953</li>
-                                                    <li class="list-group-item">21) 1E905-0129</li>
-                                                    <li class="list-group-item">22) 1E905-0129</li>
-                                                    <li class="list-group-item">23) 1E905-0129</li>
-                                                    <li class="list-group-item">24) 1E905-0129</li>
-                                                    <li class="list-group-item">25) 1E905-0129</li>
-                                                    <li class="list-group-item">26) 1E905-0129</li>
-                                                    <li class="list-group-item">27) 1G775-0112</li>
-                                                    <li class="list-group-item">28) 1G700-2224</li>
-                                                    <li class="list-group-item">29) 1G700-2224</li>
-                                                    <li class="list-group-item">30) 1G700-2224</li>
-                                                    <li class="list-group-item">31) 1G700-2224</li>
-                                                    <li class="list-group-item">32) 1G700-2224</li>
-                                                    <li class="list-group-item">33) 1G700-2224</li>
-                                                    <li class="list-group-item">34) 1G700-2224</li>
-                                                    <li class="list-group-item">35) 1G700-2224</li>
-                                                    <li class="list-group-item">36) 1G700-2224</li>
-                                                    <li class="list-group-item">37) 1G700-2224</li>
-                                                    <li class="list-group-item">38) 1G700-2224</li>
-                                                    <li class="list-group-item">39) 1G700-2224</li>
-                                                </ul>
+                <h2>  <strong style="color:#0e0e2f;">
+                <p>เรียน : ทุกท่าน</p>
+                <p>จาก : ระบบแจ้งเตือนอัตโนมัติ</p>
+                </strong></h2>
+                <p>'.nl2br($description).'</p>
+                    <p>ระบบขอแจ้งให้ทราบว่า มีรายการ ECN รายการใหม่ ของวันที่ '.date("d/m/Y" , strtotime("-1days")).' จำนวน '.count($result_ecn_create_date ).'  รายการ ดังนี้</p>
+                                        <ul class="list-group">';
+                                        $i = 1;
+                                        foreach ($result_ecn_create_date as $row) : 
+                                        $ecn_no_yesterday= $row['ecn_no'];
+                                        $email_content.='     
+                                                    <li class="list-group-item">'.$i.')'.$ecn_no_yesterday.'</li>
+                                                ';
+                                        $i++;
+                                            endforeach; 
+                                        $email_content.='     
+                                        </ul>
                                         <br> 
-
-                                        <p>รายการ ECN ที่ใกล้ถึงเวลา Effective Date ใน 30 วัน จำนวน 3 รายการ ดังนี้</p>
-                                        <ul class="list-group">
-                                                                                    <li class="list-group-item">1) 1G700-1953  Effective Date : 16/05/2019 </li>
-                                                                                     <li class="list-group-item">2) 1G700-1953  Effective Date : 30/05/2019 </li>
-                                                                                     <li class="list-group-item">3) 1E905-0129  Effective Date : 30/05/2019 </li>
-                                                                                 </ul>
+                                        <p>รายการ ECN ที่ใกล้ถึงเวลา Effective Date ในอีก '.$eff_exp_date_int.' วัน จำนวน '.count($result_noti).' รายการ ดังนี้</p>
+                                        <ul class="list-group">';
+                                        $i = 1;
+                                        foreach ($result_noti as $row) : 
+                                            $ecn_no_eff= $row['ecn_no'];
+                                            $ecn_eff_date= $row['eff_date'];
+                                            $email_content.='     
+                                            <li class="list-group-item">'.$i.') '.$ecn_no_eff.'  Effective Date : '.date('d/m/Y' , strtotime($ecn_eff_date)).'</li>
+                                                ';
+                                            $i++;
+                                            endforeach; 
+                                        $email_content.='    
+                                             </ul>
                                         <br> 
 
                                         <!-- <p>รายการ ECN ที่เลยเวลา Effective Date  ของวันที่ 16/05/2019 มีรายการค้างอยู่ xx รายการ ดังนี้</p> -->
                     </div>
                     </div>
 
-                   <div class="form-group row">
-                      <label class="col-sm-3 col-form-label">Footer : <span class="text-danger">*</span></label>
-                      <div class="col-sm-9">
-                      <textarea name="footer" rows="5" class="form-control form-control-sm">ข้อความนี้ถูกส่งจากระบบ ECN System หากมีข้อสงสัย กรุณาติดต่อที่คุณเป้ โทร 098-xxx-xxxx Email info@kubota.com</textarea>
-                    </div>
-                    </div>
+                    <p>'.nl2br($footer).'</p>
 
                        
         <div style="background: #3b434c;color: #a2abb7;padding:10px;">
             <div style="text-align:center"> 
-            <img src="https://cdn.freebiesupply.com/logos/large/2x/kubota-logo-png-transparent.png"
-             style="width: 80px;">
-                BBTV Phonebook Version 1.0.0 ©2018
+             <span>Copyright &copy; ECN Management '.date('Y').' | v. '.$app['version'].' | Last update '.$app['updated'].'</span>
             </div>
         </div>
     </body>
@@ -197,6 +201,7 @@ if(!empty($email_receiver)){
         echo '<h1 class="text-primary">ส่งอีเมลสำเร็จ</h1>';
         echo" ระบบได้ส่งข้อความไปเรียบร้อย";
     }   
+}else{
     echo '<h1 class="text-danger">ไม่มีทีอีเมลผู้รับ</h1>';
     echo" กรุณาระบบอีเมลผู้รับ";
 }
