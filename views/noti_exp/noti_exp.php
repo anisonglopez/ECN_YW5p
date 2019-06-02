575e33b980bd515c458c1d46bc500e1dfa644cb4
phoom <phoom@ch7.com>
2019-06-02 23:58:45 +0700

    noti

--- views/noti_exp/noti_exp.php
+++ views/noti_exp/noti_exp.php
@@ -62,47 +62,47 @@ endif;
           <th rowspan="2"><p>Status ECN</p></th>
           <th rowspan="2"><p>Effective</p></th>
             <th rowspan="2"><p>Eff Date</p></th>
-          <th rowspan="2"><p>Last Updated By</p></th>
+          <!-- <th rowspan="2"><p>Last Updated By</p></th> -->
           <th rowspan="2" ><p>Create Date</p></th>
             <th rowspan="2"><p>ECN No.</p></th>
             <th rowspan="2"><p>Buddle Code</p></th>
-            <th rowspan="2"><p>MINOR</p></th>
+            <!-- <th rowspan="2"><p>MINOR</p></th> -->
             <th rowspan="2"><p>Part No Old.</p></th>
-            <th rowspan="2"><p>Part  Name Old</p></th>
+            <!-- <th rowspan="2"><p>Part  Name Old</p></th> -->
             <th rowspan="2"><p>Part No. New</p></th>
             <th rowspan="2"><p>Part Name New</p></th>
-            <th rowspan="2"><p>AC</p></th>
-            <th rowspan="2" class="model_concern"><p>Model Concern</p></th>
+            <!-- <th rowspan="2"><p>AC</p></th> -->
+            <!-- <th rowspan="2" class="model_concern"><p>Model Concern</p></th> -->
             <th rowspan="2"><p>Reason</p></th>
             <!-- <th rowspan="2" ><p>New part/Full compatible/Non</p></th> -->
             <th rowspan="2"><p>WH Management</p></th>
-            <th rowspan="1" colspan="2" class="bg-warning"><p>Production</p></th>
+            <th rowspan="1" colspan="1" class="bg-warning"><p>Production</p></th>
 
 
             <!-- <th rowspan="1" colspan="2"><p>Actual for risk</p></th> -->
             <!-- <th rowspan="2"><p>Management stock (Apros)</p></th> -->
             <!-- <th rowspan="1" colspan="2"><p>Warehouse</p></th> -->
             <!-- <th rowspan="2"><p>Ddate</p></th> -->
-            <th rowspan="1" colspan="5"  class="bg-warning"><p>Follow Up Point</p></th>
-            <th rowspan="2"><p>Buyer</p></th>      
+            <!-- <th rowspan="1" colspan="5"  class="bg-warning"><p>Follow Up Point</p></th> -->
+            <!-- <th rowspan="2"><p>Buyer</p></th>      
             <th rowspan="2"><p>Supplier</p></th>     
             <th rowspan="2"><p>First PO</p></th>     
             <th rowspan="2"><p>First Deliver</p></th>      
-            <th rowspan="2"><p>Remark/Action</p></th>      
+            <th rowspan="2"><p>Remark/Action</p></th>       -->
           </tr>
           <tr class="text-center">
               <!-- <th><p>Prod Plan</p></th> -->
-              <th><p>Serial No.Break?(Y?N)</p></th>
+              <!-- <th><p>Serial No.Break?(Y?N)</p></th> -->
               <th><p>Serial No.Break</p></th>
               <!-- <th><p>Planing</p></th>
               <th><p>Wearhouse</p></th> -->
               <!-- <th><p>Supply date</p></th>
               <th><p>Serial No.</p></th> -->
-              <th><p>DWG.</p></th>
+              <!-- <th><p>DWG.</p></th>
               <th><p>Stock Supplier</p></th>
               <th><p>Cost Supplier</p></th>
               <th><p>QA Audit</p></th>
-              <th><p>Special Request</p></th>
+              <th><p>Special Request</p></th> -->
           </tr>
         </thead>
         <tbody>
@@ -115,40 +115,51 @@ endif;
         ?>
             <tr class="small">
             <td class="text-center"><a href="../ecn/ecn_change.php?id=<?php echo base64_encode($row["ecn_id"]); ?>" class="btn btn-outline-warning btn-sm"><span class="fas fa-edit fa-fw"></span></a></td>
-            <td class="text-center"><?=$row['ecn_status'] == 'Closed' ? 
-                '<span class="badge badge-pill badge-success">'.$row['ecn_status'].'</span>' 
-                : 
-                '<span class="badge badge-pill badge-danger">'.$row['ecn_status'].'</span>' ?></td>
+            <td class="text-center">
+            <?php
+                            if($row['ecn_status'] == 'Closed' ){
+                                echo  '<span class="badge badge-pill badge-success">'.$row['ecn_status'].'</span>' ;
+                            }
+                           else if($row['ecn_status'] == 'Set_Meeting' ){
+                                    echo  '<span class="badge badge-pill badge-primary">'.$row['ecn_status'].'</span>' ;
+                           }else if($row['ecn_status'] == 'Pending' ){
+                            echo  '<span class="badge badge-pill badge-warning">'.$row['ecn_status'].'</span>' ;
+                           }
+                           else{
+                               echo  '<span class="badge badge-pill badge-danger">'.$row['ecn_status'].'</span>';
+                           }
+                    ?>
+            </td>
               <td class="text-center"><?=$row['eff'] == 'Effective' ? 
                 '<span class="badge badge-pill badge-dark">'.$row['eff'].'</span>' 
                 : 
                 '<span class="badge badge-pill badge-light">'.$row['eff'].'</span>' ?></td>
                 <td><?=$eff_date?></td>
-                <td><?=$row['ecn_updated_by']?></td>
+                <!-- <td><?=$row['ecn_updated_by']?></td> -->
                 <td width="40"><?=date('d/m/Y' , strtotime($row['created_date']))?></td>
                 <td><?=$row['ecn_no']?></td>
                 <td><?=$row['buddle_code']?></td>
-                <td><?=$row['minor']?></td>
+                <!-- <td><?=$row['minor']?></td> -->
                 <td><?=$row['part_no_old']?></td>
-                <td><?=$row['part_name_old']?></td>
+                <!-- <td><?=$row['part_name_old']?></td> -->
                 <td><?=$row['part_no_new']?></td>
                 <td><?=$row['part_name_new']?></td>
-                <td><?=$row['ac']?></td>
-                <td><?=nl2br($row['model_concern'])?></td>
+                <!-- <td><?=$row['ac']?></td> -->
+                <!-- <td><?=nl2br($row['model_concern'])?></td> -->
                 <td><?=nl2br($row['reason'])?></td>
                 <td><?=nl2br($row['wh_m'])?></td>
-                <td class="text-center"><?=$row['sn_break_condit']?></td>
+                <!-- <td class="text-center"><?=$row['sn_break_condit']?></td> -->
                 <td><?=$row['sn_break']?></td>
-                <td><?=$row['dwg']?></td>
+                <!-- <td><?=$row['dwg']?></td>
                 <td><?=$row['stock_sup']?></td>
                 <td><?=$row['cost_sup']?></td>
                 <td><?=$row['qa_audit']?></td>
-                <td><?=$row['sp_req']?></td>
-                <td><?=$row['buyer']?></td>
+                <td><?=$row['sp_req']?></td> -->
+                <!-- <td><?=$row['buyer']?></td>
                 <td><?=$row['sup']?></td>
                 <td><?=$row['first_po']?></td>
                 <td><?=date('d/m/Y' , strtotime($row['first_deliver']))?></td>
-                <td><?=nl2br($row['remark'])?></td>
+                <td><?=nl2br($row['remark'])?></td> -->
 
             </tr>
             <?php endforeach; ?>
